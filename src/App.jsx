2e138480481136fdcce8e83cf55b790af153642b
Ethan Wood <woodethx@gmail.com>
2025-07-28 15:13:16 -0500

    Added education and got it live updating

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,73 @@
 import { useState } from 'react'
 import './App.css'
 import BasicInfo from './components/basicInfo';
+import Education from './components/Education';
 
 function App() {
   const [count, setCount] = useState(0);
-  const [basic, setBasic] = useState({name: "Ethan", location: "College Station, TX", phone: "(979) 571-6202", email: "woodethx@gmail.com"})
+  const [basic, setBasic] = useState({
+    name: "Ethan", 
+    location: "College Station, TX", 
+    phone: "(979) 571-6202", 
+    email: "woodethx@gmail.com"});
+  const [edu, setEdu] = useState([{
+    id: crypto.randomUUID(), 
+    school: "Texas A&M University", 
+    location: "College Station, TX", 
+    major: "Technology Management", 
+    grad: "December 2024", 
+    gpa: "3.2/4.0"}])
   
   function handleBasic(updated){
     setBasic(updated);
   }
+  function handleEdu(idU, updated){
+    const newList = edu.map(item => {
+          return item.id === idU ? updated : item
+        })
+    setEdu(newList);
+  }
+  function addEdu(){
+    const newArr = [...edu, {
+              key: crypto.randomUUID(),
+              id: crypto.randomUUID(), 
+              school: "", 
+              location: "", 
+              major: "", 
+              grad: "", 
+              gpa: ""}]
+    setEdu(newArr);
+  }
+  
+  const allEdu = edu.map((d) => {
+    return (<Education key={d.id} id={d.id} data={d} onEduChange={handleEdu}/>);
+  });
+
+  const dispEdu = edu.map((d) => {
+    return (<>
+    <b>{d.school}</b>
+    <p>{d.location}</p>
+    <i>{d.major}</i>
+    <p>{d.grad}</p>
+    <p>{d.gpa}</p>
+    </>
+    );
+  });
 
   return (
     <>
       <aside>
         <BasicInfo data={basic} onBasicChange={handleBasic}/>
+        {allEdu}
+        <button onClick={addEdu}>+ Add Education</button>
       </aside>
       <main>
         <div className="page">
           <h1>{basic.name}</h1>
           <p>{basic.location}    |    {basic.phone}    |    {basic.email}</p>
+          <hr></hr>
+          <h2>Education</h2>
+          {dispEdu}
         </div>
       </main>
     </>
---
+++ src/components/Education.jsx
@@ -0,0 +1,52 @@
+import '../styles/education.css'
+import { useState } from 'react'
+
+export default function Education({id, data, onEduChange}){
+    const [editing, setEditing] = useState(false);
+
+    const toggleEdit = () => {
+        editing ? setEditing(false) : setEditing(true);
+    }
+   
+
+    if(!editing){
+        return (
+        <div className="EduInfo">  
+            <p>School: {data.school}</p>
+            <p>Location: {data.location}</p>
+            <p>Major: {data.major}</p>
+            <p>Graduation Date: {data.grad}</p>
+            <p>GPA: {data.gpa}</p>
+            <button onClick={toggleEdit} >Edit</button>
+        </div>
+        )
+    }
+    if(editing){
+        return(
+        <div className="EduInfoEdit">  
+            <label htmlFor="school">School</label>
+            <input id="school" type="text" 
+            value={data.school}
+            onChange={(e) => onEduChange(id, {...data, school: e.target.value})}/>
+            <label htmlFor="loc">Location</label>
+            <input id="loc" type="text" 
+            value={data.location}
+            onChange={(e) => onEduChange(id, {...data, location: e.target.value})}/>
+            <label htmlFor="major">Major</label>
+            <input id="major" type="text"
+            value={data.major}
+            onChange={(e) => onEduChange(id, {...data, major: e.target.value})} />
+            <label htmlFor="grad">Graduation Date</label>
+            <input id="grad" type="text" 
+            value={data.grad}
+            onChange={(e) => onEduChange(id, {...data, grad: e.target.value})}/>
+            <label htmlFor="gpa">GPA</label>
+            <input id="gpa" type="text" 
+            value={data.gpa}
+            onChange={(e) => onEduChange(id, {...data, gpa: e.target.value})}/>
+            <button onClick={toggleEdit} >Finish</button>
+        </div>
+    )
+    }
+    
+}
